be8200443e74684527ba0afe1482e701953892a8
philippeSimier <philaure@wanadoo.fr>
2019-12-11 22:27:04 +0100

    add interruptions

--- Blink_Builtin/Blink_Builtin.ino
+++ Blink_Builtin/Blink_Builtin.ino
@@ -14,8 +14,8 @@ void setup()
 
 void loop() 
 {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^1);   // turn the LED 
   delay(200);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^1);    // turn the LED 
   delay(1000);                       // wait for a second
 }
