e0465e70c9c66ab5ffe988b693b212ce26128eb5
philippeSimier <philaure@wanadoo.fr>
2020-01-05 21:53:11 +0100

    add thingSpeak

---
+++ 10_thingSpeak/ReadField/secrets.h
@@ -0,0 +1,10 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Livebox-5648"		
+#define SECRET_PASS "vz9Lcc2RnTTmnDuD4Y"	
+
+#define SECRET_CH_ID_WEATHER_STATION 539387	     //Tessé weather station
+
+#define SECRET_CH_ID_COUNTER 566173 					//Test Ruche Danemark
+#define SECRET_READ_APIKEY_COUNTER "NWIA1TIPGT1L9S39"	//API Key for Test channel
