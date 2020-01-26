8b8cb61a252157c404e1fd203723bc5a67dd6028
psimier <philippe.simier@ac-nantes.fr>
2020-01-26 10:23:03 +0100

    add SigFoxSerieBridge

---
+++ 03_Serial/SigFoxSerieBridge/SigFoxSerieBridge.ino
@@ -0,0 +1,37 @@
+/*
+   SigFox
+*/
+#include <HardwareSerial.h>
+
+#define LED 22
+
+
+HardwareSerial serialSig(2);
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  serialSig.begin(9600, SERIAL_8N1, 26, 27);
+
+  
+  pinMode(LED, OUTPUT);
+}
+
+
+void loop() {
+  
+  char car;
+  
+  if (Serial.available() > 0) {
+    car = Serial.read();
+    serialSig.write(car);
+  }
+
+  if (serialSig.available() > 0) {
+    car = serialSig.read();
+    Serial.write(car);
+  }
+  
+}
