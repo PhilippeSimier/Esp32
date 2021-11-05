7b5a2a289b0ca975f80f71396f03875ebd6cf3be
philippeSimier <philaure@wanadoo.fr>
2021-11-05 17:55:16 +0100

    example platformio

---
+++ 01_blink_GPIO/test_blink_platformio/src/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: philippe
+ *
+ * Created on 5 novembre 2021, 17:42
+ */
+
+#include <Arduino.h>
+
+int LED = 2;
+
+void setup() 
+{
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Setup done");
+  
+}
+
+void loop() 
+{
+  digitalWrite(LED, digitalRead(LED) ^1);   // turn the LED 
+  delay(200);                       // wait for 0.2 second
+  digitalWrite(LED, digitalRead(LED) ^1);    // turn the LED 
+  delay(2000);                       // wait for 2 seconds
+}
+
