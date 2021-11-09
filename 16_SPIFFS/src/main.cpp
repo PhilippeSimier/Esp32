b11d79b341da9361f099ecf1886b83bf848c594f
philippeSimier <philaure@wanadoo.fr>
2021-11-09 16:23:33 +0100

    add SPIFFS

---
+++ 16_SPIFFS/src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include "SPIFFS.h"
+ 
+void setup() {
+  Serial.begin(115200);
+
+  // Démarre le système de fichier SPIFFS
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  // Ouvre le fichier de test en lecture seule
+  File file = SPIFFS.open("/index.html", "r");
+  if(!file){
+    // File not found | le fichier de test n'existe pas
+    Serial.println("Failed to open index.html file");
+    return;
+  }
+
+  // Envoie le contenu du fichier index.html sur le port série
+  Serial.println();
+  Serial.println("Read test.txt file content:");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
+}
+ 
+void loop() {
+}
