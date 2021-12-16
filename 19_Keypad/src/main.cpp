24586a8c17647ba192cbed59a7be7680787b3e7a
philippeSimier <philaure@wanadoo.fr>
2021-12-16 11:13:52 +0100

    add digicode

---
+++ 19_Keypad/src/main.cpp
@@ -0,0 +1,71 @@
+/*
+ *   Installer la bibliothèque Keypad avec la commande suivante
+ *   pio lib install 165
+ *   Vous devez obtenir la ligne suivante
+ *   Library Manager: Keypad @ 3.1.1 has been installed!
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: psimier
+ *
+ * Created on 16 décembre 2021, 08:50
+ */
+
+#include <Arduino.h>
+#include <Keypad.h>
+
+
+const byte ROWS = 4; // Déclaration du nombre de lignes
+const byte COLS = 3; // Déclaration du nombre de  colonnes
+
+// Déclaration des caractères sur les touches
+char keys[ROWS][COLS] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'},
+    {'*', '0', '#'}
+};
+
+//affectation des GPIO au R1,R2,R3,R4 du clavier
+byte rowPins[ROWS] = {23, 22, 3, 21};
+
+//affectation des GPIO au C1,C2,C3 du clavier
+byte colPins[COLS] = {19, 18, 5};
+
+Keypad clavier = Keypad((char *) keys, rowPins, colPins, ROWS, COLS);
+
+String code;
+String secret = "1234#";
+
+
+void setup() {
+    Serial.begin(115200);
+    pinMode( 2, OUTPUT);   // led builtin
+    
+}
+
+void loop() {
+    char key = clavier.getKey();
+
+    if (key) {
+        Serial.print(key);
+        code.concat(key);
+        if (key == '#'){
+            if (!code.compareTo(secret)) {
+                Serial.println("\nLa porte s'ouvre");
+                code = "";
+                digitalWrite(2, HIGH);
+                delay(2000);
+                digitalWrite(2, LOW);
+                
+            } else {
+                Serial.println("\nCode faux !!!");
+                code = "";
+
+            }
+        }    
+    }
+
+}
+
