f971b7cee4b30d36d6c60f24feba16052d440605
philippeSimier <philaure@wanadoo.fr>
2022-01-13 15:45:18 +0100

    add bridge GPS

---
+++ 03_Serial/Bridge_GPS_Ballon/src/main.cpp
@@ -0,0 +1,29 @@
+/*
+        Gps pour le ballon BYONICS GPS5 HA
+*/
+
+#include <HardwareSerial.h>
+
+#define LED 22
+
+HardwareSerial serialGps(2);
+
+void setup() {
+  Serial.begin(115800);
+  serialGps.begin(4800, SERIAL_8N1, 16, 17);
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED, OUTPUT);
+}
+
+
+void loop() {
+
+  char car;
+
+  if (serialGps.available() > 0) {
+    car = serialGps.read();
+    Serial.write(car);
+  }
+
+}
