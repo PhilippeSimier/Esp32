9a44976d615cae9aa749082773159f78db6afa6b
philippeSimier <philaure@wanadoo.fr>
2022-01-13 11:55:45 +0100

    add bridge_sigfox with qtcreator

--- 03_Serial/Bridge_Sigfox/src/main.cpp
+++ 03_Serial/Bridge_Sigfox/src/main.cpp
@@ -6,6 +6,10 @@ HardwareSerial serialSigfox(2); // Déclaration d'un controleur lié à UART 2
 
 void setup() {
   Serial.begin(115200);
+
+  // Carte Ruche connectée
+  // GPIO16 Rx_esp32
+  // GPIO17 Tx_esp32
   serialSigfox.begin(9600, SERIAL_8N1, 16, 17);
   delay(1000);
   Serial.println("Sigfox Serial Bridge");
