022a883a9c96aebd0be3e1bcba4475b3ab79ca39
philippeSimier <philaure@wanadoo.fr>
2022-01-13 15:11:20 +0100

    add bridge sigfox for ballon

--- 03_Serial/Bridge_Sigfox/src/main.cpp
+++ 03_Serial/Bridge_Sigfox/src/main.cpp
@@ -7,9 +7,9 @@ HardwareSerial serialSigfox(2); // Déclaration d'un controleur lié à UART 2
 void setup() {
   Serial.begin(115200);
 
-  // Carte Ruche connectée
-  // GPIO16 Rx_esp32
-  // GPIO17 Tx_esp32
+  // Carte Ruche connectée         Carte Ballon
+  // Rx_esp32    GPIO16             GPIO27
+  // Tx_esp32    GPIO17             GPIO26
   serialSigfox.begin(9600, SERIAL_8N1, 16, 17);
   delay(1000);
   Serial.println("Sigfox Serial Bridge");
---
+++ 03_Serial/Bridge_Sigfox_Ballon/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+
+
+    HardwareSerial serialSigfox(2); // Déclaration d'un controleur lié à UART 2
+
+    void setup() {
+        Serial.begin(115200);
+
+        // Carte Ruche connectée         Carte Ballon
+        // Rx_esp32    GPIO16             GPIO27
+        // Tx_esp32    GPIO17             GPIO26
+
+        serialSigfox.begin(9600, SERIAL_8N1, 27, 26);
+        delay(1000);
+        Serial.println("Sigfox Serial Bridge");
+
+        pinMode(02, OUTPUT);
+    }
+
+    void loop() {
+
+        char car;
+
+        if (Serial.available() > 0) {
+            car = Serial.read();
+            serialSigfox.write(car);
+            Serial.write(car);
+        }
+
+        if (serialSigfox.available() > 0) {
+            car = serialSigfox.read();
+            Serial.write(car);
+        }
+
+    }
