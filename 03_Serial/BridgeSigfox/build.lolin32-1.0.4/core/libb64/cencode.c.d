fb168a84f3ec9126114c73b0ee4aeb2075e43897
philippeSimier <philaure@wanadoo.fr>
2022-01-13 11:43:56 +0100

    add bridge_sigfox with qtcreator

--- 03_Serial/BridgeSigfox/build.lolin32-1.0.4/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-build.lolin32-1.0.4/core/libb64/cencode.c.o: \
- /opt/Esp8266-Arduino-Makefile/esp32-1.0.4/cores/esp32/libb64/cencode.c \
- /opt/Esp8266-Arduino-Makefile/esp32-1.0.4/cores/esp32/libb64/cencode.h
---
+++ 03_Serial/Bridge_Sigfox/src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+
+
+HardwareSerial serialSigfox(2); // Déclaration d'un controleur lié à UART 2
+
+void setup() {
+  Serial.begin(115200);
+  serialSigfox.begin(9600, SERIAL_8N1, 16, 17);
+  delay(1000);
+  Serial.println("Sigfox Serial Bridge");
+
+  pinMode(02, OUTPUT);
+}
+
+void loop() {
+
+  char car;
+
+  if (Serial.available() > 0) {
+    car = Serial.read();
+    serialSigfox.write(car);
+    Serial.write(car);
+  }
+
+  if (serialSigfox.available() > 0) {
+    car = serialSigfox.read();
+    Serial.write(car);
+  }
+
+}
