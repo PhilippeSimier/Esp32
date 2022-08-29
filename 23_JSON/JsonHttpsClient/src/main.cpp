e1e0f7af51f2a5c056f0bb8864a43f7bf720dd31
philippeSimier <philaure@wanadoo.fr>
2022-08-29 20:33:56 +0200

    improve fct lambda

--- 23_JSON/JsonHttpsClient/src/main.cpp
+++ 23_JSON/JsonHttpsClient/src/main.cpp
@@ -13,28 +13,39 @@
 #include <ArduinoJson.h>
 #include "constantes.h"
 
+bool networkOk {false};
+
 void setup() {
 
     Serial.begin(115200);
 
-    Serial.print("Connexion au WiFi ");
-    Serial.println(ssid);
+
+    WiFi.onEvent(
+            [&](WiFiEvent_t event, arduino_event_info_t info) mutable -> void {
+                switch (event) {
+                    case ARDUINO_EVENT_WIFI_READY:
+                        Serial.println("Wifi ready");
+                        break;
+                    case ARDUINO_EVENT_WIFI_STA_START:
+                        Serial.println("Wifi station start");
+                        break;
+                    case ARDUINO_EVENT_WIFI_STA_CONNECTED:
+                        Serial.println("Wifi station connected");
+                        break;
+                    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+                        Serial.print("Wifi station has got IP  : ");
+                        Serial.println(WiFi.localIP());
+                        Serial.print("Adresse IP de la gateway : ");
+                        Serial.println(WiFi.gatewayIP());
+                        networkOk = true;
+                        break;
+                    default:
+                        break;
+                }
+            });
+
 
     WiFi.begin(ssid, password); // On se connecte
-    while (WiFi.status() != WL_CONNECTED) { // On attend
-        delay(500);
-        Serial.print(".");
-    }
-
-    // Affichage des paramètres  
-    Serial.println("\r\nWiFi connecté");
-    Serial.print("Adresse IP du module ESP32 : ");
-    Serial.println(WiFi.localIP());
-    Serial.print("Adresse IP de la gateway : ");
-    Serial.println(WiFi.gatewayIP());
-
-
-
 
 
 }
@@ -42,39 +53,43 @@ void setup() {
 void loop() {
     // On se place dans le rôle du  client en utilisant HTTPClient
     HTTPClient http;
+    
+    if (networkOk) {
+        http.begin("https://philippes.ddns.net/cgi-bin/bme280Json", root_ca); //Specify the URL and certificate
+        int httpCode = http.GET(); //Make the request
 
-    http.begin("https://philippes.ddns.net/cgi-bin/bme280Json", root_ca); //Specify the URL and certificate
-    int httpCode = http.GET(); //Make the request
+        if (httpCode == 200) { // le code de status doit être OK 200
 
-    if (httpCode == 200) { // le code de status doit être OK 200
+            String mesureJson = http.getString();
+            // Serial.println(payload);
 
-        String mesureJson = http.getString();
-        // Serial.println(payload);
+            const size_t capacity = 256;
+            DynamicJsonDocument mesure(capacity);
 
-        const size_t capacity = 256;
-        DynamicJsonDocument mesure(capacity);
-
-        DeserializationError error = deserializeJson(mesure, mesureJson);
-        // Serial.printf("Memory usage : %d\n", mesure.memoryUsage()); 
-        if (error) {
-            Serial.print("deserializeJson() failed: ");
-            Serial.println(error.f_str());
-        } else {
-            bool ok = mesure["OK"];
-            if (ok) {
-                auto temp  = mesure["TemperatureC"].as<const char*>();
-                Serial.println(temp);
-            }else{
-                Serial.println("Problème capteur BME280");
+            DeserializationError error = deserializeJson(mesure, mesureJson);
+            // Serial.printf("Memory usage : %d\n", mesure.memoryUsage()); 
+            if (error) {
+                Serial.print("deserializeJson() failed: ");
+                Serial.println(error.f_str());
+            } else {
+                bool ok = mesure["OK"];
+                if (ok) {
+                    auto temp = mesure["TemperatureC"].as<const char*>();
+                    Serial.println(temp);
+                } else {
+                    Serial.println("Problème capteur BME280");
+                }
             }
+
+
+        } else {
+            Serial.printf("Error on HTTP request code : %d\n", httpCode);
         }
 
-
-    } else {
-        Serial.printf("Error on HTTP request code : %d\n", httpCode);
+        http.end();
+    }else{
+            Serial.println("Pas de réseau");
     }
-
-    http.end();
     delay(10000);
 
 }
